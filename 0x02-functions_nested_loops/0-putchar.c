5ba0bd6ab8fee0669e1dda549d49b29864327dfb
Taofeeq Taofeeqat Olawumi <taofeeqolawumi@gmail.com>
2022-06-08 07:11:28 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *    
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ *  * print_alphabet - Entry point
+ *   * Description: a function that prints the alphabet,
+ *    * in lowecase followed by a new line
+ *     * Return: void
+ *      */
+void print_alphabet(void)
 {
-		return (write(1, &c, 1));
-}
+		char c;
 
+			for (c = 'a'; c <= 'z'; c++)
+						_putchar(c);
+				_putchar('\n');
+}
