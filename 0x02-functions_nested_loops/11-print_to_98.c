d59caa31f59532e5b0b384318a3d4146462caabe
Taofeeq Taofeeqat Olawumi <taofeeqolawumi@gmail.com>
2022-06-08 13:30:11 -0700

    98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,32 @@
-#include "holberton.h"
 #include <stdio.h>
+#include "main.h"
+
 /**
- * print_to_98 - print to 98
- * @n : number to start from
- * Return: 0 or 1
+ * print_to_98 - prints numbers to 98
+ * @n: starting intege
+ *
+ * Description: prints all natural number from n - 98
+ *
+ * Return: void
  */
 
 void print_to_98(int n)
 {
-
-	while (n < 98)
+	if (n < 98)
 	{
-		printf("%i, ", n);
-		n++;
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
 	}
-	while (n > 98)
+	else if (n > 98)
 	{
-		printf("%i, ", n);
-		n--;
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
 	}
-
-	printf("98");
-	putchar('\n');
+	printf("98\n");
 }
