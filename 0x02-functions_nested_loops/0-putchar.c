d65ab79d9b8a31d3efd5ecb1f7066bb290d8fb1c
Taofeeq Taofeeqat Olawumi <taofeeqolawumi@gmail.com>
2022-06-08 08:43:35 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,15 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabet,
+ * in lowercase followed by a new line
+ * Return: void
  */
-int main(void)
+void print_alphabet(void)
 {
-	putchar(");
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+		_putchar(c);
 	_putchar('\n');
-	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef main_h
-#define main_h
+
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,6 +12,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
+void print_times_table(int n);;
