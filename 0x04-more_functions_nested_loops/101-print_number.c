3362f861ef08e30748cd1ca09d34a44d291a0e55
Taofeeq Taofeeqat Olawumi <taofeeqolawumi@gmail.com>
2022-06-09 07:42:35 -0700

    101printnum

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,45 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - checks for checks for a digit (0 through 9).
- *  @n: n -  Variable
- *  Return: Always 0.
+ * print_number - prints an integer
+ * @n: input integer parameter
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int z;
-	int m, b;
+	unsigned int i = n;
 
-	b = 10;
-
-	if (n < 10 && n >= 0)
+	if (n < 0)
 	{
-		_putchar (n + '0');
+		_putchar(45);
+		i = -i;
 	}
-	else if (n > -10 && n < 0)
+	if (i / 10)
 	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
+		print_number(i / 10);
 	}
-
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-		while (z / b > 9)
-		{
-			b = b * 10;
-		}
-		while (b > 0)
-		{
-			m = z / b;
-			z = z % b;
-			_putchar (m + '0');
-			b = b / 10;
-		}
-	}
+	_putchar(i % 10 + '0');
+}
