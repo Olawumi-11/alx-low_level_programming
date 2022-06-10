9570dbb048a85ad685ce014d2be0b4ba538dbc16
Taofeeq Taofeeqat Olawumi <taofeeqolawumi@gmail.com>
2022-06-10 04:07:07 -0700

    hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,47 +1,27 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minutes of the day
+ * jack_bauer - prints every minutes of the day of
+ * Jack Bauer, starting from 00:00 to 23:59.
  *
- * Description:prints every minute of the day
- *
- * Return: void
+ * Return: 24 hour clock line by line
  */
 
 void jack_bauer(void)
 {
+	int h, m;
 
-		int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+	for (h = 0; h < 24; h++)
+	{
 
-
-		hours_max = 58;
-		hours_tens = '0';
-		while (hours_tens < '3')
+		for (m = 0; m < 60; m++)
 		{
-			if (hours_tens == '2')
-			{
-				hours_max = '4';
-			}
-			hours_ones = '0';
-			while (hours_ones < hours_max)
-			{
-				minutes_tens = '0';
-				while (minutes_ones < 58)
-				{
-					_putchar(hours_tens);
-					_putchar(hours_ones);
-					_putchar(':');
-					_putchar(minutes_tens);
-					_putchar(minutes_ones);
-					_putchar('\n');
-					minutes_ones++;
-				}
-				minutes_ones = '0';
-				minutes_tens++;
-			}
-			minutes_tens = '0';
-			hours_ones++;
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
-		hours_ones = '0';
-		hours_tens++;
+	}
 }
