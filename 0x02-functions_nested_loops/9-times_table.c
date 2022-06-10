ca3269263e753ff41490081f5066ec0a1c7e3945
Taofeeq Taofeeqat Olawumi <taofeeqolawumi@gmail.com>
2022-06-10 03:58:08 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * times_table - prints times table
@@ -7,39 +7,35 @@
 
 void times_table(void)
 {
+	int row, column, product, tens, ones;
 
-	int a = 0;
-	int b;
-	int rep;
-	while (a <= 9)
+	for (row = 0; row <= 9; row++)
 	{
-		b = 0;
-		while (b <= 9)
+		for (column = 0; column <= 9; column++)
 		{
-			rep = a * b;
-			if (b == 0)
-			{
-				_putchar('0' + rep);
-			}
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar('0' + rep);
-			}
-			else
-			{
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
 
-			if (b < 9)
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
 			}
-			b++;
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
 		}
 		_putchar('\n');
-		a++;
 	}
 }
