6e1c5033c2fcc1f7845524dd6246e2bf27f8b783
Taofeeq Taofeeqat Olawumi <taofeeqolawumi@gmail.com>
2022-06-16 01:17:54 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,9 +5,9 @@
  *            an inputted number of bytes from src.
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest.
- *  @n: The number of bytes from src to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
  *
- *  Return: A pointer to the resulting string dest.
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
