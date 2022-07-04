cfc2dbe3c67f74f9a8557abeb95ee2204ae47f9d
Taofeeq Taofeeqat Olawumi <taofeeqolawumi@gmail.com>
2022-07-04 03:16:22 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
